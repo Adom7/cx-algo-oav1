8e78d375a5c80a5ca2370205741602e1aee0e942
unknown <adlaneould@gmail.com>
2020-10-16 15:56:16 +0200

    Adding e01.js file

--- e03.js
+++ e03.js
@@ -1,5 +1,15 @@
 function marsos(str){
-            return str.match(/.{1,3}/g);
+            const chunk = str.match(/.{1,3}/g);
+                for (let i = 83; i <= 90; i++) {
+                  let letter = String.fromCharCode(i);
+              
+                  if (!str.toUpperCase().includes(letter)) {
+                    return false
+                  }
+                }
+              
+                return true
+              
 };
 
 
